79f11a8bfcc3a5d74e26b29cfb7765c34886b9c2
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-05-03 20:03:38 +0530

    #include<stdio.h> int main() {     int n,d,k,t=0,s=0,j,c=0,m=0;     scanf("%d",&n);     while(n>0)     {         d=n%10;         s=s+d;         n=n/10;     }     while(s>0)     {         k=s%10;         t=t+k;         c++;         s=s/10;     }     if(c==2)     {         while(t>0)         {             j=t%10;             m=m+j;             t=t/10;         }         printf("%d",m);     }     else     {         printf("%d",t);    } }

---
+++ Add_Digits.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,d,k,t=0,s=0,j,c=0,m=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        d=n%10;
+        s=s+d;
+        n=n/10;
+    }
+    while(s>0)
+    {
+        k=s%10;
+        t=t+k;
+        c++;
+        s=s/10;
+    }
+    if(c==2)
+    {
+        while(t>0)
+        {
+            j=t%10;
+            m=m+j;
+            t=t/10;
+        }
+        printf("%d",m);
+    }
+    else
+    {
+        printf("%d",t);
+   }
+}
