91620ff17ca4906b48fe511f060caf5622a044cc
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-05-05 22:06:17 +0530

    #include<stdio.h> int main() {     int n,arr[100][100],k=0,i,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         for(j=0;j<2;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<n;i++)     {         k=0;         for(j=0;j<2;j++)         {             k=k+arr[i][j];         }         printf("%d ",k);     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100][100],k=0,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        k=0;
+        for(j=0;j<2;j++)
+        {
+            k=k+arr[i][j];
+        }
+        printf("%d
+",k);
+    }
+}
