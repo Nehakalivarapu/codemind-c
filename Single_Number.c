9ff0af96c229adcfae3bff5bf00e33062020c1f1
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-06-09 10:58:13 +0530

    #include<stdio.h> int main() {     int n,a[200],i,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]==-1)         {             continue;         }         for(j=i+1;j<n;j++)         {             if(a[i]==a[j])             {                 a[i]=-1;                 a[j]=-1;             }         }     }     for(i=0;i<n;i++)     {         if(a[i]!=-1)         {             printf("%d",a[i]);         }     } }

---
+++ Single_Number.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[200],i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==-1)
+        {
+            continue;
+        }
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                a[i]=-1;
+                a[j]=-1;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]!=-1)
+        {
+            printf("%d",a[i]);
+        }
+    }
+}
