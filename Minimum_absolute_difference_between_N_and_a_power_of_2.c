da323d06c5aab3199ab1cb9ef1b8b49a1db119e3
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-06-22 11:05:44 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,j,min=0,max=0,p;     scanf("%d",&n);     for(i=0;i<n;i++)     {         if(n>=pow(2,i))         {             min=i;         }         if(n<pow(2,i))         {             max=i;             break;         }     }     if(n-pow(2,min)<=(pow(2,max)-n))     {         p=n-pow(2,min);         printf("%d",p);     }     else     {         p=pow(2,max)-n;         printf("%d",p);     } }

---
+++ Minimum_absolute_difference_between_N_and_a_power_of_2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,j,min=0,max=0,p;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        if(n>=pow(2,i))
+        {
+            min=i;
+        }
+        if(n<pow(2,i))
+        {
+            max=i;
+            break;
+        }
+    }
+    if(n-pow(2,min)<=(pow(2,max)-n))
+    {
+        p=n-pow(2,min);
+        printf("%d",p);
+    }
+    else
+    {
+        p=pow(2,max)-n;
+        printf("%d",p);
+    }
+}
