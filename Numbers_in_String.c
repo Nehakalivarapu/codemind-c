ac3a6270d0d2ccf9b34c731803caef7b9b024405
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-06-29 23:09:22 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[100];     int i,len,s=0;     scanf("%[^ ]s",str);     len=strlen(str);     for(i=0;i<len;i++)     {         if(str[i]>='0' && str[i]<='9')         {             s=s+str[i];             s=s-48;
            }
        }
        printf("%d",s);
    }

---
+++ Numbers_in_String.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    int i,len,s=0;
+    scanf("%[^
+]s",str);
+    len=strlen(str);
+    for(i=0;i<len;i++)
+    {
+        if(str[i]>='0' && str[i]<='9')
+        {
+            s=s+str[i];
+            s=s-48;
+            
+        }
+    }
+    printf("%d",s);
+}
