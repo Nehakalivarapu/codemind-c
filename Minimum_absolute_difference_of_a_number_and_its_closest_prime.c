da9236ba35180e138f1c9cdd1af80b387dc4a18c
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-06-30 09:59:44 +0530

    #include<stdio.h> int main() {     int i,j,a,t,c=0,p1=0,p2=0,k;     scanf("%d",&a);     for(k=a;k>2;k--)     {         c=0;         for(j=2;j<k;j++)         {             if(k%j==0)             {                 c=1;                 break;             }         }         if(c==0)         {             p1=k;             break;         }     }     for(k=a;k<a+100;k++)     {         c=0;         for(j=2;j<k;j++)         {             if(k%j==0)             {                 c=1;                 break;             }
            }
            if(c==0)
            {
                p2=k;
                break;
            }
            
        }
        if(a-p1<=p2-a)
        {
            printf("%d
    ",a-p1);
        }
        else
        {
            printf("%d
    ",p2-a);
        }
    }

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,a,t,c=0,p1=0,p2=0,k;
+    scanf("%d",&a);
+    for(k=a;k>2;k--)
+    {
+        c=0;
+        for(j=2;j<k;j++)
+        {
+            if(k%j==0)
+            {
+                c=1;
+                break;
+            }
+        }
+        if(c==0)
+        {
+            p1=k;
+            break;
+        }
+    }
+    for(k=a;k<a+100;k++)
+    {
+        c=0;
+        for(j=2;j<k;j++)
+        {
+            if(k%j==0)
+            {
+                c=1;
+                break;
+            }
+            
+        }
+        if(c==0)
+        {
+            p2=k;
+            break;
+        }
+        
+    }
+    if(a-p1<=p2-a)
+    {
+        printf("%d
+",a-p1);
+    }
+    else
+    {
+        printf("%d
+",p2-a);
+    }
+}
