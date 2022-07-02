84821088b1f05b4ec45474e3d67c01c5ee72f1fc
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-07-02 19:18:36 +0530

    #include<stdio.h> int main()  {      int n,a[20],i,j,s=0;      scanf("%d",&n);      for(i=0;i<n;i++)      {          scanf("%d",&a[i]);      }         if(n%2==0)         {             for(i=0;i<n;i++)             {             printf("%d ",a[i]);             }         }         else         {             for(i=0;i<n;i++)             {                 printf("%d ",a[i]);             }             printf("0");         }  }

---
+++ Elements_with_pairs.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+ {
+     int n,a[20],i,j,s=0;
+     scanf("%d",&n);
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&a[i]);
+     }
+        if(n%2==0)
+        {
+            for(i=0;i<n;i++)
+            {
+            printf("%d ",a[i]);
+            }
+        }
+        else
+        {
+            for(i=0;i<n;i++)
+            {
+                printf("%d ",a[i]);
+            }
+            printf("0");
+        }
+ }
