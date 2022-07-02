b13eb0fa7e481f55053b1117ddc6be18ce20711c
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-07-02 19:23:08 +0530

    #include<stdio.h> int main()  {      int n,a[20],i,j,s=0;      scanf("%d",&n);      for(i=0;i<n;i++)      {          scanf("%d",&a[i]);      }
         for(i=0;i<n;i++)
         {
             for(j=i+1;j<n;j++)
             {
                 if(a[i]==a[j])
                 {  
                     a[i]=-1;
                 }
             }
         }
         for(i=0;i<n;i++)
         {
             if(a[i]==-1)
             {
                 continue;
             }
             else
             {
                 if(a[i]%2==0)
                 {
                     s++;
                 }
             }
         }
         printf("%d",s);
     }

---
+++ unique_even_count.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+ {
+     int n,a[20],i,j,s=0;
+     scanf("%d",&n);
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&a[i]);
+     }
+        
+     for(i=0;i<n;i++)
+     {
+         for(j=i+1;j<n;j++)
+         {
+             if(a[i]==a[j])
+             {  
+                 a[i]=-1;
+             }
+         }
+     }
+     for(i=0;i<n;i++)
+     {
+         if(a[i]==-1)
+         {
+             continue;
+         }
+         else
+         {
+             if(a[i]%2==0)
+             {
+                 s++;
+             }
+         }
+     }
+     printf("%d",s);
+ }
