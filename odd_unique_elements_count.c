1e7c6e25df3729425e3f384dad436c0624e19af6
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-07-02 19:11:30 +0530

    #include<stdio.h> int main() {     int n,p=0,a[100],c=0,i,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         p=0;         if(a[i]==-1)         {             continue;         }         for(j=i+1;j<n;j++)         {             if(a[i]==a[j])             {                 p=1;                 a[j]=-1;             }
            }
            for(i=0;i<n;i++)
            {
                if(a[i]==-1)
                {
                    continue;
                }
                if(a[i]%2!=0)
                {
                    c++;
                }
            }
            printf("%d",c);
        }
    }

---
+++ odd_unique_elements_count.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n,p=0,a[100],c=0,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        p=0;
+        if(a[i]==-1)
+        {
+            continue;
+        }
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                p=1;
+                a[j]=-1;
+            }
+            
+        }
+        for(i=0;i<n;i++)
+        {
+            if(a[i]==-1)
+            {
+                continue;
+            }
+            if(a[i]%2!=0)
+            {
+                c++;
+            }
+        }
+        printf("%d",c);
+    }
+}
