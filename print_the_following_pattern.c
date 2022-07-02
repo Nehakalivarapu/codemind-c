37eecc9fdf418358dad57af2461d67b95c044edf
Nehakalivarapu <98727399+Nehakalivarapu@users.noreply.github.com>
2022-07-02 15:20:41 +0530

    #include<stdio.h> int main() {     int i,j,k,l,n;     scanf("%d",&n);     k=65+n-1;     l=n;     for(i=1;i<=n;i++)     {         for(j=1;j<=l;j++)         {             printf("%c ",k);         }         l--;         k--;         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
+    int i,j,k,l,n;
     scanf("%d",&n);
+    k=65+n-1;
+    l=n;
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n;j++)
-        { 
-            printf("%c ",'A'-1+i);
+        for(j=1;j<=l;j++)
+        {
+            printf("%c ",k);
         }
+        l--;
+        k--;
         printf("
 ");
     }
